c152e9cb68e4d5e89ebe4fff264bd80db3c6e04c
abrorm3 <abrormukhammadiev@gmail.com>
2023-08-16 05:23:23 +0500

    test

--- fakeUserDataGenerator/src/app/user-data-generator/user.service.ts
+++ fakeUserDataGenerator/src/app/user-data-generator/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {faker, fakerRU, fakerKA_GE, fakerJA} from "@faker-js/faker";
+// import {faker, fakerRU, fakerKA_GE, fakerJA} from "@faker-js/faker";
 
 
 @Injectable({
@@ -7,20 +7,20 @@ import {faker, fakerRU, fakerKA_GE, fakerJA} from "@faker-js/faker";
 })
 export class UserService {
   generateUserData(lang: string) {
-    const langModule = lang === 'RU' ? fakerRU : lang === 'KA_GE' ? fakerKA_GE : lang === 'JA' ? fakerJA: faker;
+    // const langModule = lang === 'RU' ? fakerRU : lang === 'KA_GE' ? fakerKA_GE : lang === 'JA' ? fakerJA: faker;
 
     const userData: any[] = [];
 
-    for (let i = 0; i < 30; i++) {
-      const user = {
-        id: i + 1,
-        name: langModule.person.fullName(),
-        address: langModule.location.streetAddress(),
-        phone: langModule.phone.number(),
-      };
+    // for (let i = 0; i < 30; i++) {
+    //   const user = {
+    //     id: i + 1,
+    //     name: langModule.person.fullName(),
+    //     address: langModule.location.streetAddress(),
+    //     phone: langModule.phone.number(),
+    //   };
 
-      userData.push(user);
-    }
+    //   userData.push(user);
+    // }
 
     return userData;
   }
